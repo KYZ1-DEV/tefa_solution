13bacb5c675e50885e7562de80025376f50ee3af
Rezkybanh <rezkymaulanamuhammad00@gmail.com>
2024-09-24 12:08:49 +0700

    Menyelesaikan dashboard industri menu editProfile

--- resources/views/industri/profile/password.blade.php
+++ resources/views/industri/profile/password.blade.php
@@ -1,50 +1,73 @@
 @extends('dashboard/index')
+
 @section('navItem')
     <x-industri></x-industri>
 @endsection
+
 @section('profile')
     {{ route('profileIndustri') }}
 @endsection
+
 @section('main')
-                    <!-- Begin Page Content -->
-                    <div class="container-fluid">
-                        @if (Session::get('success'))
-                    <div class="alert alert-success alert-dismissible fade show">
-                        <ul>
-                            <li>{{ Session::get('success') }}</li>
-                        </ul>
-                    </div>
-                @endif
-
-
-            <!-- Page Heading -->
-            <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                <h1 class="h3 mb-0 text-gray-800">Edit Password</h1>
-            </div>
-         
-            <!-- Content Row -->
-            <div class="row">
-                <div class="card shadow mb-4">
-                    <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Password</h6>
-                    </div>
-                    <div class="card-body">
-                        <div class="text-center">
-                            <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
-                                src="{{ asset('dashboard/img/undraw_posting_photo.svg') }}" alt="...">
-                        </div>
-                        <p>Add some quality, svg illustrations to your project courtesy of <a
-                                target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a
-                            constantly updated collection of beautiful svg images that you can use
-                            completely free and without attribution!</p>
-                        <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on
-                            unDraw &rarr;</a>
-                    </div>
-                </div>
-
-            </div>
-
+<!-- Begin Page Content -->
+<div class="container-fluid">
+    @if (Session::get('success'))
+        <div class="alert alert-success alert-dismissible fade show">
+            <ul>
+                <li>{{ Session::get('success') }}</li>
+            </ul>
         </div>
-        <!-- /.container-fluid -->
-@endsection
+    @endif
 
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Profile</h1>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+        <div class="col-12"> <!-- Menggunakan col-12 untuk memastikan card memenuhi layar -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Edit Password</h6>
+                </div>
+                <div class="card-body">
+                    <form action="{{ url('/industri/editPassword') }}" method="post" class="p-4">
+                        @method('PUT')
+                        @csrf
+                        <input type="hidden" name="id" value="{{ Auth::user()->id }}">
+                        <div class="row align-items-center">
+                            <div class="col-md-3">
+                                <label for="newPassword" class="form-label">Password Baru</label>
+                            </div>
+                            <div class="col-md-9">
+                                <input type="password" class="form-control @error('newPassword') is-invalid @enderror" id="newPassword" name="newPassword" placeholder="Masukkan password baru">
+                                @error('newPassword')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row align-items-center mt-3">
+                            <div class="col-md-3">
+                                <label for="newPassword_confirmation" class="form-label">Konfirmasi Password</label>
+                            </div>
+                            <div class="col-md-9">
+                                <input type="password" class="form-control" id="newPassword_confirmation" name="newPassword_confirmation" placeholder="Konfirmasi password baru">
+                            </div>
+                        </div>
+
+                        <div class="text-center mt-3">
+                            <button type="submit" class="btn btn-primary ">Simpan</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- /.container-fluid -->
+@endsection
--- resources/views/pointakses/industri/index.blade.php
+++ resources/views/pointakses/industri/index.blade.php
@@ -1,141 +1,128 @@
 @extends('dashboard/index')
+
 @section('navItem')
     <x-industri></x-industri>
 @endsection
+
 @section('profile')
     {{ route('profileIndustri') }}
 @endsection
+
 @section('main')
-                    <!-- Begin Page Content -->
-                <div class="container-fluid">
-                                @if (Session::get('success'))
-                            <div class="alert alert-success alert-dismissible fade show">
-                                <ul>
-                                    <li>{{ Session::get('success') }}</li>
-                                </ul>
+    <!-- Begin Page Content -->
+    <div class="container-fluid">
+        @if (Session::get('success'))
+            <div class="alert alert-success alert-dismissible fade show">
+                <ul>
+                    <li>{{ Session::get('success') }}</li>
+                </ul>
+            </div>
+        @endif
+
+        <!-- Page Heading -->
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
+            <a href="#" class="btn btn-sm btn-primary shadow-sm d-none d-sm-inline-block">
+                <i class="fas fa-download fa-sm text-white-50"></i> Generate Report
+            </a>
+        </div>
+
+        <!-- Content Row for Menu Cards -->
+        <div class="row mb-4">
+            <!-- Edit Profile Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <a href="{{ route('profileIndustri') }}" class="text-decoration-none">
+                    <div class="card shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <i class="fas fa-user-edit fa-2x text-gray-500"></i>
+                                </div>
+                                <div class="col ml-2">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Edit Profile</div>
+                                </div>
                             </div>
-                        @endif
-
-
-                    <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                        </div>
                     </div>
+                </a>
+            </div>
 
-                    {{-- <!-- Content Row -->
-                    <div class="row">
-
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                {{ Auth::user()->email }}</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ Auth::user()->role }}</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
+            <!-- Monitoring Bantuan Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <a href="{{ route('monitoringBantuan') }}" class="text-decoration-none">
+                    <div class="card shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <i class="fas fa-eye fa-2x text-gray-500"></i>
+                                </div>
+                                <div class="col ml-2">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Monitoring Bantuan</div>
                                 </div>
                             </div>
                         </div>
-
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-                                                Earnings (Annual)</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">$215,000</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Earnings (Monthly) Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-info shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-                                            </div>
-                                            <div class="row no-gutters align-items-center">
-                                                <div class="col-auto">
-                                                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-                                                </div>
-                                                <div class="col">
-                                                    <div class="progress progress-sm mr-2">
-                                                        <div class="progress-bar bg-info" role="progressbar"
-                                                            style="width: 50%" aria-valuenow="50" aria-valuemin="0"
-                                                            aria-valuemax="100"></div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Pending Requests Card Example -->
-                        <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-warning shadow h-100 py-2">
-                                <div class="card-body">
-                                    <div class="row no-gutters align-items-center">
-                                        <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                                Pending Requests</div>
-                                            <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-                                        </div>
-                                        <div class="col-auto">
-                                            <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div> --}}
-
-                 
-                    <!-- Content Row -->
-                    <div class="row">
-                        <div class="card shadow mb-4">
-                            <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-primary">Illustrations</h6>
-                            </div>
-                            <div class="card-body">
-                                <div class="text-center">
-                                    <img class="img-fluid px-3 px-sm-4 mt-3 mb-4" style="width: 25rem;"
-                                        src="{{ asset('dashboard/img/undraw_posting_photo.svg') }}" alt="...">
-                                </div>
-                                <p>Add some quality, svg illustrations to your project courtesy of <a
-                                        target="_blank" rel="nofollow" href="https://undraw.co/">unDraw</a>, a
-                                    constantly updated collection of beautiful svg images that you can use
-                                    completely free and without attribution!</p>
-                                <a target="_blank" rel="nofollow" href="https://undraw.co/">Browse Illustrations on
-                                    unDraw &rarr;</a>
-                            </div>
-                        </div>
-
-
                     </div>
+                </a>
+            </div>
 
-                </div>
-                <!-- /.container-fluid -->
+            <!-- List Sekolah Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <a href="{{ route('listSekolah') }}" class="text-decoration-none">
+                    <div class="card shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <i class="fas fa-school fa-2x text-gray-500"></i>
+                                </div>
+                                <div class="col ml-2">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">List Sekolah</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <!-- Laporan Card -->
+            <div class="col-xl-3 col-md-6 mb-4">
+                <a href="{{ route('laporan') }}" class="text-decoration-none">
+                    <div class="card shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col-auto">
+                                    <i class="fas fa-file-alt fa-2x text-gray-500"></i>
+                                </div>
+                                <div class="col ml-2">
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Laporan</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <!-- Welcome Message -->
+        <div class="text-center mb-4">
+            <h1 id="welcome-message" class="display-4 text-gray-800"></h1>
+        </div>
+    </div>
+    <!-- /.container-fluid -->
+
+
+    <script>
+        const message = "Selamat Datang, Industri!";
+        let index = 0;
+        const welcomeMessageElement = document.getElementById("welcome-message");
+
+        function typeMessage() {
+            if (index < message.length) {
+                welcomeMessageElement.textContent += message.charAt(index);
+                index++;
+                setTimeout(typeMessage, 100); // Delay in milliseconds
+            }
+        }
+
+        window.onload = typeMessage; // Start typing effect when page loads
+    </script>
 @endsection
-
